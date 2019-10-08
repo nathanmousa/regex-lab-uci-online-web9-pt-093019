992542ece321c9070597919cfe9bf83afbc715d5
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-08 18:30:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,22 +1,24 @@
 def starts_with_a_vowel?(word)
-  
   if word.match(/\b[aeiou]/i)
     return true
   else
     return false
   end
-  
 end
+
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
   text.scan(/un\S*ing/i)
-
 end
+
+
 
 def words_five_letters_long(text)
   text.scan(/\b[a-z]{5}\b/i)
 end
+
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\b[A-Z.]\w*[.,!()]/)
@@ -25,6 +27,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
     return false
   end
 end
+
+
 
 def valid_phone_number?(phone)
   if tex.match(//)
