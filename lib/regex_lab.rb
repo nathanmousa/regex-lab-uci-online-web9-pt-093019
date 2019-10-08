0e9870cec2fe3248dcc78ad3ce19ec7b5368877b
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-08 16:14:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,16 @@
 def starts_with_a_vowel?(word)
+  
   if word.match(/\b[aeiou]/i)
     return true
   else
     return false
   end
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+
+  
 
 end
 
